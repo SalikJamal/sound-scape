194a9e7b8fe60547eccc2fc77a6a9dbe3868c1ee
Salik <iamsalikjamal@gmail.com>
2024-02-21 22:17:30 +0500

    Completed box component

--- components/Box.tsx
+++ components/Box.tsx
@@ -1,11 +1,14 @@
+import { twMerge } from "tailwind-merge"
+
 interface BoxProps {
     children: React.ReactNode;
     className?: string;
 }
 
+
 export default function Box({ children, className }: BoxProps) {
     return (
-        <div>
+        <div className={twMerge("bg-neutral-900 rounded-lg h-fit w-full", className)}>
             {children}
         </div>
     )
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -34,7 +34,18 @@ export default function Sidebar({ children }: SidebarProps) {
         <div className="flex h-full">
             <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
                 <Box>
-                    Sidebar Navigation
+                    <div className="flex flex-col gap-y-4 px-5 py-4">
+                        {routes.map((route) => (
+                            <SidebarItem 
+                                key={route.label}
+                                {...route}
+                            />
+                        ))}
+                    </div>
+                    
+                </Box>
+                <Box className="overflow-y-auto h-full">
+                    Song Library
                 </Box>
             </div>
         </div>
