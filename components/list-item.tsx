108496987355bcd50fc9af48ab777aefb48b7f39
Salik <iamsalikjamal@gmail.com>
2024-02-24 10:02:04 +0500

    Layout complete

--- components/list-item.tsx
+++ components/list-item.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image"
 import { useRouter } from "next/navigation"
+import { FaPlay } from "react-icons/fa"
 
 interface IListItemProps {
     image: string;
@@ -14,13 +15,16 @@ export default function ListItem({ image, name, href }: IListItemProps) {
     
     const router = useRouter()
     
-    const onClick = () => {
+    const handleClick = () => {
         // Add authentication before push
         router.push(href)
     }
 
     return (
-        <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+        <button 
+            className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4"
+            onClick={handleClick}
+        >
             <div className="relative min-h-[64px] min-w-[64px]">
                 <Image
                     className="object-cover"
@@ -29,7 +33,12 @@ export default function ListItem({ image, name, href }: IListItemProps) {
                     fill
                 />
             </div>
-            <p>{name}</p>
+            <p className="font-medium truncate py-5">
+                {name}
+            </p>
+            <div className="absolute transition opacity-0 rounded-full flex items-center justify-center bg-green-500 p-4 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110">
+                <FaPlay className="text-black" />
+            </div>
         </button>
     )
 }
