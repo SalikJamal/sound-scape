a2adea582bd0b9c90d4065a3eacb1a1e57592246
Salik <iamsalikjamal@gmail.com>
2024-02-27 11:17:04 +0500

    Updated next.config.js

--- components/song-item.tsx
+++ components/song-item.tsx
@@ -1,6 +1,8 @@
 "use client"
 
+import useLoadImage from "@/hooks/useLoadImage"
 import { ISong } from "@/types/types"
+import Image from "next/image";
 
 interface ISongItemProps {
     data: ISong;
@@ -9,9 +11,22 @@ interface ISongItemProps {
 
 
 export default function SongItem({ data, onClick }: ISongItemProps) {
+    
+    const imagePath = useLoadImage(data)
+
     return (
-        <div>
-            
+        <div
+            className="relative group flex flex-col items-center justify-center rounded-md overflow-hidden gap-x-4 bg-neutral-400/5 cursor-pointer hover:bg-neutral-400/10 transition p-3" 
+            onClick={() => onClick(data.id)}
+        >
+            <div className="relative aspect-square w-full h-full rounded-md overflow-hidden">
+                <Image
+                    className="object-cover"
+                    src={imagePath || "/images/liked.png"}
+                    fill
+                    alt="Song Cover"
+                />
+            </div>
         </div>
     )
 }
