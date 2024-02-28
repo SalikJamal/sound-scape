e8254063830e360c438aa165d300dfffdfe1a3ea
Salik <iamsalikjamal@gmail.com>
2024-02-28 11:29:54 +0500

    Music player

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import UserProvider from "@/providers/UserProvider"
 import ModalMProvider from "@/providers/ModalProvider"
 import ToasterProvider from "@/providers/ToasterProvider"
 import getSongsByUserId from "@/actions/getSongsByUserId"
+import Player from "@/components/player"
 
 const figTree = Figtree({ subsets: ["latin"] })
 
@@ -34,6 +35,7 @@ export default async function RootLayout({
               <Sidebar songs={userSongs}>
                 {children}
               </Sidebar>
+              <Player />
             </UserProvider>
           </SupabaseProvider>
       </body>
