aadb4445f291db48488b7f0cc98e6c27ec59a4fc
Salik <iamsalikjamal@gmail.com>
2024-02-29 18:19:18 +0500

    Favicon configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import ToasterProvider from "@/providers/ToasterProvider"
 import getSongsByUserId from "@/actions/getSongsByUserId"
 import Player from "@/components/player"
 import getActiveProductsWithPrices from "@/actions/getActiveProductsWithPrices"
+import Head from "next/head"
 
 const figTree = Figtree({ subsets: ["latin"] })
 
@@ -29,6 +30,15 @@ export default async function RootLayout({
   const products = await getActiveProductsWithPrices()
   return (
     <html lang="en">
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <body className={figTree.className}>
         <ToasterProvider />
           <SupabaseProvider>
