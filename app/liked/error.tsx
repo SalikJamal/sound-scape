bf366d1b5ffa8c03b140cbc51436c1bcf66eca85
Salik <iamsalikjamal@gmail.com>
2024-02-29 18:34:47 +0500

    renaming

--- app/liked/error.tsx
+++ app/liked/error.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import Box from "@/components/box"
+import Box from "@/components/boxes"
 
 
 export default function Error() {
--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import Box from "@/components/box"
+import Box from "@/components/boxes"
 import { BounceLoader } from "react-spinners"
 
 
